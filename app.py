d38035d9854d3f833c11751fe5305896fa949352
anthonyespino <55596700+anthonyespino@users.noreply.github.com>
2024-12-18 20:31:17 -0800

    updates w dylan

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
 
 @app.route('/review', methods=['GET', 'POST'])
 def review_posts():
-    admin_password = "n^As6DMra@!jL#`=hU{3;"  # Secure admin password
+    admin_password = "Iamtooc00l!"  # Secure admin password
     if request.args.get('password') != admin_password:
         return "Unauthorized access", 401
 
